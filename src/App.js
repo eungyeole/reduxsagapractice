949884bd6fde5c3ebffa7c8fcc708b3b8d7ea6b0
안은결 <aneungyeol@an-eungyeol-ui-MacBookPro.local>
2020-12-14 22:47:28 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import CounterContainer from './containers/CounterContainer.js';
 
 function App() {
   return (
     <>
-      sd
+      <CounterContainer />
     </>
   );
 }
